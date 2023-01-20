2e68f7fc6971fdd80871296c751e5178acad825f
Rodrigo Gonzalez <rodrigo.gonzaleztr@outlook.com>
2023-01-19 21:25:50 -0300

    Se agregaron dos nuevos tipos de fuentes: -Beaufort(titulos) -Spiegel(texto comun)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ const Header = () => {
                         <a className="navbar-brand" href="https://www.leagueoflegends.com/es-mx/">
                             <img src="logoLol.svg" width={'150px'} alt="lolLogo" />
                         </a>
+                        
                         <form className="d-flex" role="search">
                             <button className="button-52 button-nav" role="button" type="submit" disabled>Disabled button</button>
                             <button className="button-52 button-nav" role="button" type="submit" disabled>Disabled button</button>
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,6 +1,7 @@
 const Video = () => {
     return (
         <section className="video-container">
+            <h1 className="title-header">League of Legends Champions</h1>
             <video src="warriors-lol-cinematic.mp4" autoPlay muted loop></video>
         </section>
     )
